7697d885d27a535080f97c963c7671ef1cc1dac3
Gitnana00 <0818uzimihs@gmail.com>
2024-07-01 16:48:15 +0900

    API疎通確認DONE

--- frontend/src/Test.js
+++ frontend/src/Test.js
@@ -1,8 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 const Test = () => {
+
+  const [memos, setMemos] = useState([]);
+
+  // 先ほど作成したLaravelのAPIのURL
+  const url = "http://localhost/api/memos";
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(url);
+        console.log(res)
+        setMemos(res.data.memos);
+        return;
+      } catch (e) {
+        return e;
+      }
+    })();
+  }, []);
 
   return (
     <>
-      <p>これはテストです。</p>
+      {memos.map((memo)=> {
+        return (
+          <div key={memo.id}>
+            <h1>{memo.subject}</h1>
+            <p>{memo.content}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
